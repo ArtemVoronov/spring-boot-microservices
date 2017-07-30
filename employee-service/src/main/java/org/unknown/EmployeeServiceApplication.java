9a68b4306c05414afb66a89dc35b37106399fb07
voronov <git@eyeline.mobi>
2017-07-30 21:52:30 +0700

    added integration employee and eureka services

--- employee-service/src/main/java/org/unknown/EmployeeServiceApplication.java
+++ employee-service/src/main/java/org/unknown/EmployeeServiceApplication.java
@@ -11,36 +11,48 @@ import org.unknown.model.employee.Employee;
 import org.unknown.model.employee.EmployeeRepository;
 import org.unknown.model.position.Position;
 import org.unknown.model.position.PositionRepository;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
+@EnableDiscoveryClient
 @SpringBootApplication
+@RestController
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeServiceApplication.class, args);
 	}
 
+  //TODO: clean
+
+  @RequestMapping("/greeting")
+  public String greeting() {
+    return "Hello from EurekaClient!";
+  }
+
 	//TODO: clean
-//  @Bean
-//  public CommandLineRunner demo(DepartmentRepository departmentRepository, PositionRepository positionRepository,
-//                                EmployeeRepository employeeRepository) {
-//    return (args) -> {
-//      Department prod = departmentRepository.save(new Department("Production"));
-//      departmentRepository.save(new Department("QA"));
-//      departmentRepository.save(new Department("Marketing"));
-//      departmentRepository.save(new Department("Support"));
-//
-//
-//      Position programmer = positionRepository.save(new Position("Software engineer"));
-//      positionRepository.save(new Position("Support engineer"));
-//      positionRepository.save(new Position("QA engineer"));
-//      Position boss = positionRepository.save(new Position("Boss"));
-//
-//      employeeRepository.save(new Employee("Bob", new Date(), boss, prod, 1100000));
-//      employeeRepository.save(new Employee("Tom", new Date(), programmer, prod, 500000));
-//      employeeRepository.save(new Employee("Joe", new Date(), programmer, prod, 400000));
-//
-//    };
-//  }
+  @Bean
+  public CommandLineRunner demo(DepartmentRepository departmentRepository, PositionRepository positionRepository,
+                                EmployeeRepository employeeRepository) {
+    return (args) -> {
+      Department prod = departmentRepository.save(new Department("Production"));
+      departmentRepository.save(new Department("QA"));
+      departmentRepository.save(new Department("Marketing"));
+      departmentRepository.save(new Department("Support"));
+
+
+      Position programmer = positionRepository.save(new Position("Software engineer"));
+      positionRepository.save(new Position("Support engineer"));
+      positionRepository.save(new Position("QA engineer"));
+      Position boss = positionRepository.save(new Position("Boss"));
+
+      employeeRepository.save(new Employee("Bob", new Date(), boss, prod, 1100000));
+      employeeRepository.save(new Employee("Tom", new Date(), programmer, prod, 500000));
+      employeeRepository.save(new Employee("Joe", new Date(), programmer, prod, 400000));
+
+    };
+  }
 }
